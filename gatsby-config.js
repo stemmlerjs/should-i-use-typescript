5e30ae03586c994190732c075266a7123718c4cf
Khalil Stemmler <metroidman12@gmail.com>
2019-05-16 12:54:53 -0400

    Update favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
         background_color: `#601EFF`,
         theme_color: `#601EFF`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
